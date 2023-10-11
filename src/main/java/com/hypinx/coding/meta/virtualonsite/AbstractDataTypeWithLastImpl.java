ae3f3a5ba0908a0ee99ac6bbfb6e22ce23f8b787
Dylan Patel <pdylan92@gmail.com>
2023-10-11 01:09:12 -0400

    Added 2 new meta coding problems

---
+++ src/main/java/com/hypinx/coding/meta/virtualonsite/AbstractDataTypeWithLastImpl.java
@@ -0,0 +1,167 @@
+package com.hypinx.coding.meta.virtualonsite;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// Implement the following data structure:
+interface AbstractDataTypeWithLast <K, V> {
+    /** Adds a value v to this data structure this can be accessed by get() using key k. */
+    public void put(K k, V v);
+
+    /** Returns the value associated with key k. */
+    public V get(K k);
+
+    /** Removes value for key k. */
+    public void delete(K k);
+
+    /**
+     * Returns the most recent key either added with put() or accessed
+     * with get() that hasn't been removed by delete().
+     */
+    public K last();
+}
+
+// Examples:
+/**
+ AbstractDataTypeWithLast<String, Integer> data;
+ data.put("a", 1);
+ data.put("b", 2);
+ data.last();        // => "b"
+ data.get("a");      // => 1
+ data.last();        // => "a"
+ data.delete("a");
+ data.last();        // => "b"
+*/
+
+/**
+ * Solution: AbstractDataTypeWithLastImpl is the solution class and Node is a supporting class. You do not need to create this class, you can simply start to edit the methods provided above during the actual interview. The solution is written this way because it gives compliation errors otherwise.
+ *
+ * Follow Up Questions:
+ *
+ * 1. Can you think of some edge cases that might be a risk to this structure? What kind of edge cases do we need to account for?
+ *
+ * Concurrency: If the data structure is accessed and modified concurrently by multiple threads, you need to ensure thread safety. You might need to use synchronization mechanisms, such as locks or concurrent data structures, to prevent data corruption and race conditions.
+ *
+ * Duplicate Keys: What should happen if you attempt to put a key that already exists in the data structure? Should it update the existing value, or should it be treated as an error?
+ *
+ * Null Keys or Values: How should the data structure handle null keys or values if they are allowed in your use case? Ensure that null values or keys do not lead to unexpected behavior.
+ *
+ * Capacity and Resizing: If your HashMap reaches its load factor and triggers resizing, you need to ensure that the keys' order is maintained correctly during resizing.
+ *
+ * Load Distribution: Check how well the hash function distributes keys to avoid hash collisions, especially in cases where keys have patterns that might lead to clustering.
+ *
+ * 2. Will you ever have to worry about performing a delete when there are no nodes present? E.g. On line 143, will this cause a null pointer exception?
+ *
+ * Answer: No we don't have to worry because even if we have no nodes, if we try to run line 143 then we will simply reference the head node. We will always have head and tail present so we will not run into a null pointer issue on this line.
+ *
+ * Time Complexity:
+ *
+ * put(K k, V v): O(1)
+ *
+ * Inserting or updating a key-value pair in the HashMap (map) has an average time complexity of O(1). It also calls the appendToEnd or moveToEnd method, which has O(1) time complexity.
+ *
+ * get(K k): O(1)
+ *
+ * Retrieving a value associated with a key from the HashMap (map) has an average time complexity of O(1). It also calls the moveToEnd method, which has O(1) time complexity.
+ *
+ * delete(K k): O(1)
+ *
+ * Removing a key-value pair from the HashMap (map) has an average time complexity of O(1). It also calls the removeNode method, which has O(1) time complexity.
+ *
+ * last(): O(1)
+ *
+ * Retrieving the last key from the doubly-linked list (most recent) has O(1) time complexity.
+ *
+ * Space Complexity: O(N) - the space complexity is linear because it is dependent on the number of nodes we have which is N.
+ */
+
+class Node<K, V> {
+    K key;
+    V value;
+    Node<K, V> prev;
+    Node<K, V> next;
+
+    Node(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+public class AbstractDataTypeWithLastImpl<K,V> {
+    private Map<K, Node<K, V>> map;
+    private Node<K, V> head;
+    private Node<K, V> tail;
+
+    public AbstractDataTypeWithLastImpl () {
+        this.map = new HashMap<>();
+        this.head = new Node<>(null, null);
+        this.tail = new Node<>(null, null);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public void put(K k, V v) {
+        // If the key exists, update the value and move the node to the end
+        if (map.containsKey(k)) {
+            Node<K, V> node = map.get(k);
+            node.value = v;
+            moveToEnd(node);
+        } else {
+            // Otherwise, create a new node and append it to the end
+            Node<K, V> newNode = new Node<>(k, v);
+            map.put(k, newNode);
+            appendToEnd(newNode);
+        }
+    }
+
+    public V get(K k) {
+        if (map.containsKey(k)) {
+            Node<K, V> node = map.get(k);
+            moveToEnd(node);
+            return node.value;
+        }
+        return null;
+    }
+
+    public void delete(K k) {
+        if (map.containsKey(k)) {
+            Node<K, V> node = map.remove(k);
+            removeNode(node);
+        }
+    }
+
+    public K last() {
+        if (tail.prev != head) {
+            return tail.prev.key;
+        }
+        return null; // No keys in the data structure
+    }
+
+    private void appendToEnd(Node<K, V> node) {
+        node.prev = tail.prev;
+        node.next = tail;
+        tail.prev.next = node;
+        tail.prev = node;
+    }
+
+    private void moveToEnd(Node<K, V> node) {
+        removeNode(node);
+        appendToEnd(node);
+    }
+
+    private void removeNode(Node<K, V> node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    public static void main(String[] args) {
+        AbstractDataTypeWithLastImpl<String, Integer> data = new AbstractDataTypeWithLastImpl<>();
+        data.put("a", 1);
+        data.put("b", 2);
+        System.out.println(data.last()); // Output: "b"
+        System.out.println(data.get("a")); // Output: 1
+        System.out.println(data.last()); // Output: "a"
+        data.delete("a");
+        System.out.println(data.last()); // Output: "b"
+    }
+}
---
+++ src/main/java/com/hypinx/coding/meta/virtualonsite/TreeUniqueIds.java
@@ -0,0 +1,96 @@
+package com.hypinx.coding.meta.virtualonsite;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * You are given a tree of nodes, where each node has a collection of children and has an integer 'id' field. Write a function which takes a node and assigns each node in the tree a unique integer id
+ *
+ * Both TreeNode and TreeUniqueIds are used in the solution. The main method to run a test execution is in TreeUniqueIds, this is a sample
+ * coded by me (not provided by the interviewer)
+ *
+ * Time Complexity:
+ * The time complexity of this code is O(N), where N is the total number of nodes in the tree. This is because we visit each node exactly once in a depth-first manner. The time complexity is linear with respect to the number of nodes in the tree.
+ *
+ * Space Complexity:
+ * The space complexity is O(H), where H is the height of the tree. This space is mainly used by the function call stack during the depth-first traversal. In the worst case, if the tree is highly unbalanced and resembles a linked list, the space complexity could be O(N), where N is the number of nodes. However, for balanced trees, the space complexity would be much smaller, closer to O(log N), where log N is the height of a balanced tree.
+ *
+ * Follow Up Question: If you want to adapt the code to work with general graphs instead of trees, what are things to consider?
+ *
+ * Cycle Detection: General graphs can contain cycles. When traversing such graphs, you need to implement cycle detection to avoid infinite loops. One way to do this is to keep track of visited nodes during the traversal and prevent revisiting nodes that have already been visited.
+ *
+ * Disconnected Components: Graphs can have multiple disconnected components. You might need to ensure that all components are visited. You can achieve this by iterating through all nodes and calling the traversal from unvisited nodes until all nodes are visited.
+ *
+ * Node Visitation Order: In a tree, you can typically rely on a specific order of node visitation (e.g., pre-order or post-order traversal). In a general graph, the order in which nodes are visited can be arbitrary, depending on the graph structure and traversal algorithm used.
+ *
+ * Handling Graph Data Structure: The code provided assumes a specific data structure for the graph (i.e., the TreeNode class). In a general graph, you might be dealing with various data structures like adjacency lists or adjacency matrices. You would need to adapt your traversal algorithm to work with the specific graph representation you have.
+ *
+ * Directed vs. Undirected Graphs: Depending on whether your general graph is directed or undirected, you might need to adjust how you traverse edges and handle cycles.
+ *
+ * Graph Traversal Algorithm: In the provided code, we used a recursive depth-first traversal. For general graphs, you might consider other traversal algorithms, such as breadth-first search (BFS), depth-first search (DFS), or more advanced algorithms like Dijkstra's algorithm or A* search, depending on the specific problem you're trying to solve.
+ *
+ * Stopping Criteria: You might need to define a stopping criterion for your traversal, such as reaching a specific target node, exploring a certain number of nodes, or traversing until there are no more unvisited nodes.
+ *
+ */
+class TreeNode {
+    int id;
+    List<TreeNode> children;
+
+    TreeNode(int id) {
+        this.id = id;
+        this.children = new ArrayList<>();
+    }
+}
+
+public class TreeUniqueIds {
+    // Function to assign unique IDs to nodes in a tree
+    public static int assignUniqueIds(TreeNode root, int nextId) {
+        if (root == null) {
+            return nextId;
+        }
+
+        // Assign the current ID and increment the next available ID
+        root.id = nextId++;
+
+        // Recursively assign unique IDs to children
+        for (TreeNode child : root.children) {
+            nextId = assignUniqueIds(child, nextId);
+        }
+
+        return nextId;
+    }
+
+    public static void main(String[] args) {
+        // Create a sample tree
+        TreeNode root = new TreeNode(0);
+        TreeNode node1 = new TreeNode(0);
+        TreeNode node2 = new TreeNode(0);
+        TreeNode node3 = new TreeNode(0);
+
+        root.children.add(node1);
+        root.children.add(node2);
+        node2.children.add(node3);
+
+        // Assign unique IDs to nodes in the tree and get the next available ID
+        int nextId = assignUniqueIds(root, 1); // Start with 1
+
+        // Print the assigned IDs
+        printTree(root);
+
+        // The next available ID after assignment can be used for other purposes
+        System.out.println("Next Available ID: " + nextId);
+    }
+
+    // Function to print tree node IDs
+    public static void printTree(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.println("Node ID: " + root.id);
+
+        for (TreeNode child : root.children) {
+            printTree(child);
+        }
+    }
+}
