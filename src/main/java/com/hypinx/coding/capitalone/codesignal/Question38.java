5622c309ba7133bc0799a757df6b4aceefcc7a5d
Dylan Patel <pdylan92@gmail.com>
2024-03-25 17:10:38 -0400

    Added non-interface solution for power day coding (solutionV3) and updated 3 code signal questions

---
+++ src/main/java/com/hypinx/coding/capitalone/codesignal/Question38.java
@@ -0,0 +1,67 @@
+package com.hypinx.coding.capitalone.codesignal;
+
+import java.util.Arrays;
+import java.util.Set;
+
+public class Question38 {
+
+    public static void main(String[] args) {
+        String[] reviews = new String[]{
+                "very purple and good",
+                "very very bad one of the worst",
+                "good but worse than analogue",
+                ""
+        };
+        String[] positiveWords = new String[]{
+                "good", "best"
+        };
+        String[] negativeWords = new String[]{
+                "bad", "worse", "worst"
+        };
+        String[] intensifiers = new String[]{
+                "very"
+        };
+        String[] expected = new String[]{
+                "positive", "negative", "neutral", "neutral"
+        };
+        String[] result = solution(reviews, positiveWords, negativeWords, intensifiers);
+
+        System.out.println(Arrays.equals(expected, result));
+
+    }
+
+    public static String[] solution(String[] reviews, String[] positiveWords, String[] negativeWords, String[] intensifiers) {
+        Set<String> positiveSet = Set.of(positiveWords);
+        Set<String> negativeSet = Set.of(negativeWords);
+        Set<String> intensifierSet = Set.of(intensifiers);
+
+        String[] result = new String[reviews.length];
+
+        for (int i = 0; i < reviews.length; i++) {
+            String[] review = reviews[i].split(" ");
+            int count = 0;
+            int segment = 0;
+            for (int j = 0; j < review.length; j++) {
+                String word = review[j];
+                if (intensifierSet.contains(word)) {
+                    segment++;
+                } else if (positiveSet.contains(word)) {
+                    count = count + 1 + segment;
+                    segment = 0;
+                } else if (negativeSet.contains(word)) {
+                    count = count - 1 - segment;
+                    segment = 0;
+                }
+            }
+
+            if (count == 0) {
+                result[i] = "neutral";
+            } else if (count < 0) {
+                result[i] = "negative";
+            } else {
+                result[i] = "positive";
+            }
+        }
+        return result;
+    }
+}
---
+++ src/main/java/com/hypinx/coding/capitalone/powerday/simplebanking/solutionV3/Solution.java
@@ -0,0 +1,266 @@
+package com.hypinx.coding.capitalone.powerday.simplebanking.solutionV3;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Questions to ask:
+ *  - For this problem, we have accountId's given. Should we take into account security on this, like
+ *  should we tokenize this data as we work with it? Or we can just store as is.
+ *
+ *
+ *  -
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        String[][] operations = {
+                // Level 1 ------------------------------------------------------------------
+                {"CREATE_ACCOUNT", "account1"},
+                {"CREATE_ACCOUNT", "account1"},
+                {"CREATE_ACCOUNT", "account2"},
+                {"DEPOSIT", "non-existing", "2700"},
+                {"DEPOSIT", "account1", "2700"},
+                // ------------------------------------------------------------------
+                // Level 2 ------------------------------------------------------------------
+                {"TRANSFER", "account1", "account2", "2701"},
+                {"TRANSFER", "account1", "account2", "200"},
+                // ------------------------------------------------------------------
+                // Level 3 ------------------------------------------------------------------
+                {"TRANSFER", "account1", "account2", "2500"},
+                {"DEPOSIT", "account2", "300"},
+                {"CREATE_ACCOUNT", "account3"},
+                {"DEPOSIT", "account3", "4000"},
+                {"TOP_ACTIVITY", "3"},
+                {"DEPOSIT", "account2", "1000"},
+                {"TOP_ACTIVITY", "2"},
+                {"TOP_ACTIVITY", "5"}
+                // ------------------------------------------------------------------
+
+        };
+
+        /**
+         * I will create an object of the simplifiedBankingInMemory and then for every operation I will invoke the process operation
+         * method in this class.
+         */
+        SimplifiedBankingInMemory simplifiedBankingInMemory = new SimplifiedBankingInMemory();
+        for (String[] operation : operations) {
+            simplifiedBankingInMemory.processOperations(operation);
+        }
+    }
+}
+
+// Model class to represent a bank account
+class BankAccount {
+    // Level 1 ------------------------------------------------------------------
+    private String accountId;
+    private double balance;
+    // ------------------------------------------------------------------
+
+    // Level 3 ------------------------------------------------------------------
+    private double activityIndicator;
+    // ------------------------------------------------------------------
+
+    public BankAccount(String accountId) {
+        // Level 1 ------------------------------------------------------------------
+        this.accountId = accountId;
+        this.balance = 0;
+        // ------------------------------------------------------------------
+        // Level 3 ------------------------------------------------------------------
+        this.activityIndicator = 0;
+        // ------------------------------------------------------------------
+    }
+
+    // Level 1 ------------------------------------------------------------------
+    public String getAccountId() {
+        return this.accountId;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+    // ------------------------------------------------------------------
+
+    // Level 3 ------------------------------------------------------------------
+    public double getActivityIndicator() {
+        return this.activityIndicator;
+    }
+    // ------------------------------------------------------------------
+
+    public double adjustAccountBalance(double balance) {
+        this.balance += balance;
+        // Level 3 ------------------------------------------------------------------
+        this.activityIndicator += Math.abs(balance);
+        // ------------------------------------------------------------------
+        return this.balance;
+    }
+}
+
+/**
+ * The SimplifiedBankingInMemory class will contain the logic for the banking system
+ */
+class SimplifiedBankingInMemory {
+    /**
+     * I will use a Map to hold the in memory storage. The key will be the account id and the value will be the BankAccount object
+     */
+    private Map<String, BankAccount> accounts;
+
+    /**
+     * The constructor will initialize to an empty hash map
+     */
+    public SimplifiedBankingInMemory() {
+        this.accounts = new HashMap<>();
+    }
+
+    /**
+     * The process operations will take a string array of the operation to perform. I will use extract the operation type which is always
+     * the first argument and then use a switch statement in order to invoke the proper action. I will also define the variable for
+     * accountId and balance because these could be common in operations. I will assign the values individually in each switch case.
+     */
+    public void processOperations(String[] operation) {
+        String type = operation[0].toLowerCase();
+        String accountId;
+        double balance;
+        switch (type) {
+            // Level 1 ------------------------------------------------------------------
+            case "create_account":
+                // In the create account the accountId is in index 1 so I will assign the value here and then inside a print statement
+                // I will print the result of calling the createAccount method
+                accountId = operation[1];
+                System.out.println(this.createAccount(accountId));
+                break;
+            case "deposit":
+                // AccountId for the deposit action is again in index 1. The balance is index 2 but it's as a String data type so I will
+                // parse it into a double to pass into the deposit method.
+                accountId = operation[1];
+                balance = Double.parseDouble(operation[2]);
+                System.out.println(this.deposit(accountId, balance));
+                break;
+            // ------------------------------------------------------------------
+            // Level 2 ------------------------------------------------------------------
+            case "transfer":
+                // The fromId is index 1, toId is index 2, and balance is index 3 which we need to parse to a double
+                String fromId = operation[1], toId = operation[2];
+                balance = Double.parseDouble(operation[3]);
+                System.out.println(this.transfer(fromId, toId, balance));
+                break;
+            // ------------------------------------------------------------------
+            // Level 3 ------------------------------------------------------------------
+            case "top_activity":
+                // The n value is index 1 which is String so need to parse to an int. I'm also going to put a check here to make sure
+                // we lower bound n to be the size of the accounts map size incase its higher. Otherwise we could result in an index
+                // out of bounds exception
+                int n = Integer.parseInt(operation[1]);
+                // Ensure we don't go out of bounds
+                n = Math.min(n, accounts.size());
+                // The top activity method will return a list of accountId and the activity indicator. I am going to store into a list
+                // which then I will iterate over an print
+                List<BankAccount> sortedList = this.topActivity(n);
+                // Print the sorted account list
+                for (int i = 0; i < n; i++) {
+                    BankAccount currentAccount = sortedList.get(i);
+                    System.out.print(currentAccount.getAccountId() + "(" + currentAccount.getActivityIndicator() + ") ");
+                }
+                System.out.println();
+                break;
+            // ------------------------------------------------------------------
+            default:
+                System.out.println("Operation " + operation[0] + " not supported.");
+        }
+    }
+
+    // Level 1 ------------------------------------------------------------------
+
+    /**
+     * The createAccount method will accept accountId as an argument. The first edge case to check is to see if the account already exists.
+     * By querying the accounts map, if an entry is present it means the account exists so we can return false. Otherwise we can
+     * add a new entry to the map and set a new BankAccount object as the value and return true.
+     */
+    public boolean createAccount(String accountId) {
+
+        // Check if the account already exists
+        if (accounts.containsKey(accountId)) return false;
+
+        // Proceed to add a new account
+        accounts.put(accountId, new BankAccount(accountId));
+        return true;
+    }
+
+    /**
+     * For the deposit method we take the accountId and balance. The first edge case is if the account does not exist we have no balance
+     * to update. So we can return -1. If the account exists then we can query the map to get the account. Then invokve the
+     * adjustAccountBalance method and pass in the balance to adjust by. Then finally we can get the updated balance on the account object
+     * and return it.
+     */
+    public double deposit(String accountId, double balance) {
+
+        // If account does not exists, return -1
+        if (!accounts.containsKey(accountId)) return -1;
+
+        // Proceed to update the balance
+        BankAccount bankAccount = accounts.get(accountId);
+        bankAccount.adjustAccountBalance(balance);
+        return bankAccount.getBalance();
+    }
+    // ------------------------------------------------------------------
+
+    // Level 2 ------------------------------------------------------------------
+
+    /**
+     * The transfer method will have three arguments. The fromId, toId and balance. There's three edge cases we have to check for
+     * 1. If either account does not exist
+     * 2. If from and to account are the same
+     * 3. And if there is not enough balance in fromAccount to make the transfer
+     * <p>
+     * If all the cases pass we can proceed to make the transfer. And to make the transfer I'm going to utilize the adjustAccountBalance
+     * method which is already present from making the deposit. Since the transfer will deduct money from the fromAccount I will just
+     * take the balance and multiply by negative one and pass that in. So this will reduce the balance in fromAccount. And then finally
+     * return the balance in fromAccount after the transfer has been made
+     */
+    public double transfer(String fromId, String toId, double balance) {
+        // Validate both accounts exist
+        if (!accounts.containsKey(fromId) || !accounts.containsKey(toId)) return -1;
+        // Validate both accounts are unique
+        if (fromId.equals(toId)) return -1;
+        // Validate there are sufficient funds in from account
+        BankAccount fromAccount = accounts.get(fromId);
+        if (fromAccount.getBalance() < balance) return -1;
+
+        // Proceed to make the transfer
+        BankAccount toAccount = accounts.get(toId);
+        // Multiply by negative 1 so we can pass in the negative value of the balance to add to our existing balance
+        fromAccount.adjustAccountBalance(balance * (-1));
+        toAccount.adjustAccountBalance(balance);
+        return fromAccount.getBalance();
+    }
+    // ------------------------------------------------------------------
+
+    // Level 3 ------------------------------------------------------------------
+
+    /**
+     * The argument in the topActivity method will be an integer n, we already performed a check in the calling code so we can use it
+     * directly. For this logic we will need to go over all the accounts we have and sort based on activity indicator. So I will
+     * create a list of BankAccounts from the accounts map. Then we can use the sort method on the list and create our own comparator
+     * function.
+     * <p>
+     * In the comparator function I will first check if the activity indicators are equal, if so check the accountId and return in
+     * alphabetical order. Otherwise if activity indicator is not equal, return the object with the higher value
+     */
+    public List<BankAccount> topActivity(int n) {
+        // Extract all BankAccounts from accounts map
+        List<BankAccount> sortedList = new ArrayList<>(accounts.values());
+        sortedList.sort(((a, b) -> {
+            if (a.getActivityIndicator() == b.getActivityIndicator()) {
+                // Sort by the account id if activity indicator is equal
+                return a.getAccountId().compareTo(b.getAccountId());
+            }
+
+            // Compare by activity indicator
+            return Double.compare(b.getActivityIndicator(), a.getActivityIndicator());
+        }));
+
+        return sortedList;
+    }
+    // ------------------------------------------------------------------
+}
